9a114ef9d995fe860e37459c281090f085ee9ea7
Oguike <deraoguike67@gmail.com>
2023-02-25 06:45:37 -0800

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *
+ * main - entry point
+ *
+ * return- always return 0
+ *
+ */
+
+int main()
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
