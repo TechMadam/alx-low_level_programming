da00697af6ec1d00fb135c2770117574a6a06138
Oguike <deraoguike67@gmail.com>
2023-02-26 07:10:49 -0800

    0-putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints "_putchar" followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char c[] = "_putchar\n";
+    int i;
+
+    for (i = 0; c[i] != '\0'; i++)
+    {
+        write(1, &c[i], 1);
+    }
+
+    return (0);
+}
