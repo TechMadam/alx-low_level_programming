ffc5dd3c3bfd493e2b46f7bbc4182373c9f5d327
Oguike <deraoguike67@gmail.com>
2023-02-26 07:16:55 -0800

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,13 +8,13 @@
  */
 int main(void)
 {
-    char c[] = "_putchar\n";
-    int i;
+char c[] = "_putchar\n";
+int i;
 
-    for (i = 0; c[i] != '\0'; i++)
-    {
-        write(1, &c[i], 1);
-    }
+for (i = 0; c[i] != '\0'; i++)
+{
+ write(1, &c[i], 1);
+}
 
-    return (0);
+return (0);
 }
