a30144ae778b0f2bac7d7fbda0b2715219abe7c1
TechMadam <122824527+TechMadam@users.noreply.github.com>
2023-03-07 00:42:17 -0800

    Please enter the commit message for your changes. Lines starting

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+void positive_or_negative(int i);
+
+int largest_number(int a, int b, int c);
+
+void print_remaining_days(int month, int day, int year);
+
+int convert_day(int month, int day);
+
+
+
+
+
+#endif
